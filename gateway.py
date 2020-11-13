649f117ca54d3ac2f537a4399df3528e5892e080
MaxKuznets0v <max.kuznetsov@yahoo.com>
2020-11-13 15:51:01 +0300

    add logs

--- gateway.py
+++ gateway.py
@@ -16,6 +16,7 @@ class Gateway:
     uis_rpc = RpcProxy('uis')
     likes_rpc = RpcProxy('likes')
     favorites_rpc = RpcProxy('favorites')
+    logger_rpc = RpcProxy('logger')
 
     # Logic
 
@@ -121,8 +122,8 @@ class Gateway:
             return self._cors_response(Response(json.dumps({"message": "Wrong credentials"}), status=400), '*', 'POST, OPTIONS')
         return self._cors_response(Response(json.dumps({"token": token}), 202), '*', 'POST, OPTIONS')
 
-    @http('GET,OPTIONS', '/feed')
-    @http('GET,OPTIONS', '/feed/')
+    @http('POST,OPTIONS', '/feed')
+    @http('POST,OPTIONS', '/feed/')
     def feed_handler(self, request):
         """Getting top events for authorized user
         request parameters:
@@ -160,29 +161,33 @@ class Gateway:
             }
         """
         if request.method == 'OPTIONS':
-            return self._cors_response(Response(), '*', 'GET, OPTIONS')
+            return self._cors_response(Response(), '*', 'POST, OPTIONS')
 
-        authorized, user = self._token_validate_by_params(request)
+        authorized, user = self._token_validate_by_body(request)
         if authorized:
             # if token is invalid
             if not user:
-                return self._cors_response(Response(json.dumps({"message": "Invalid token"}), status=403), '*', 'GET, OPTIONS')
+                return self._cors_response(Response(json.dumps({"message": "Invalid token"}), status=403), '*', 'POST, OPTIONS')
 
             # if there's no tags provided
             try:
-                tags = json.loads(request.args['tags'])
+                tags = self._get_content(request)['tags']
             except KeyError:
                 tags = []
 
             try:
                 events = self.filter_rpc.get_events(user, tags)
-            finally:
-                pass
+                self.logger_rpc.log(self.name, self.feed_handler.__name__, str(request), "Info", "Got user's feed")
+            except:
+                self.logger_rpc.log(self.name, self.feed_handler.__name__, str(request), "Error", "Can't get info from filter")
+
         else:
             try:
                 events = self.event_das_rpc.get_events_by_date()
-            finally:
-                pass
+                self.logger_rpc.log(self.name, self.feed_handler.__name__, str(request), "Info", "Got feed for non-authorized user")
+            except:
+                self.logger_rpc.log(self.name, self.feed_handler.__name__, str(request), "Error", "Can't get info from event_das")
+
 
         return self._cors_response(Response(json.dumps(events, ensure_ascii=False), status=200), '*', 'GET, OPTIONS')
 
@@ -219,8 +224,11 @@ class Gateway:
         
         try:
             event = self.event_das_rpc.get_event_by_id(event_id)
-        finally:
-            return self._cors_response(Response(json.dumps(event, ensure_ascii=False), 200), '*', 'GET, OPTIONS')
+            self.logger_rpc.log(self.name, self.get_event_handler.__name__, str(request), "Info", "Got event info")
+        except:
+            self.logger_rpc.log(self.name, self.get_event_handler.__name__, str(request), "Error", "Can't get event info")
+            
+        return self._cors_response(Response(json.dumps(event, ensure_ascii=False), 200), '*', 'GET, OPTIONS')
 
     @http('POST,PUT,GET,OPTIONS', '/profile/interests')
     @http('POST,PUT,GET,OPTIONS', '/profile/interests/')
@@ -250,6 +258,7 @@ class Gateway:
             return self._cors_response(Response(json.dumps({"message": "Invalid token"}), 403), '*', 'POST, PUT, GET, OPTIONS')
 
         if request.method == 'GET':
+            self.logger_rpc.log(self.name, self.interest_handler.__name__, str(request), "Info", "Getting user's interests")
             interests = self.uis_rpc.get_weights_by_id(user)
             clean_interests = list()
             for item in interests.items():
@@ -260,11 +269,13 @@ class Gateway:
         interests = self._get_content(request)['interests']
         try:
             self.uis_rpc.create_new_q([user, interests])
-        finally:
-            if request.method == 'POST':
-                return self._cors_response(Response(json.dumps({"message": "Interests added"}), 201), '*', 'POST, PUT, GET, OPTIONS')
-            else:
-                return self._cors_response(Response(json.dumps({"message": "Interests changed"}), 200), '*', 'POST, PUT, GET, OPTIONS')
+            self.logger_rpc.log(self.name, self.interest_handler.__name__, str(request), "Info", "Interests updated")
+        except:
+            self.logger_rpc.log(self.name, self.interest_handler.__name__, str(request), "Error", "Can't change interests")
+        if request.method == 'POST':
+            return self._cors_response(Response(json.dumps({"message": "Interests added"}), 201), '*', 'POST, PUT, GET, OPTIONS')
+        else:
+            return self._cors_response(Response(json.dumps({"message": "Interests changed"}), 200), '*', 'POST, PUT, GET, OPTIONS')
 
     @http('DELETE,GET,POST,OPTIONS', '/reaction/<string:reaction_type>') 
     @http('DELETE,GET,POST,OPTIONS', '/reaction/<string:reaction_type>/')  
@@ -308,14 +319,19 @@ class Gateway:
             return self._cors_response(Response(json.dumps({"message": "Invalid token"}), 403), '*', 'DELETE, GET, POST, OPTIONS')
         
         if request.method == 'POST':
+            self.logger_rpc.log(self.name, self.reaction_handler.__name__, str(request), "Info", "Leaving reaction")
             event_id = content['event_id']
-            if reaction_type == 'like':
-                self.likes_rpc.new_like([user, event_id])
-            elif reaction_type == 'favorite':
-                self.favorites_rpc.new_fav([user, event_id])
+            try:
+                if reaction_type == 'like':
+                    self.likes_rpc.new_like([user, event_id])
+                elif reaction_type == 'favorite':
+                    self.favorites_rpc.new_fav([user, event_id])
+            except:
+                self.logger_rpc.log(self.name, self.reaction_handler.__name__, str(request), "Error", "Can't leave reaction")
 
             return self._cors_response(Response(json.dumps({"message": "Reaction committed"}), 200), '*', 'DELETE, GET, POST, OPTIONS')
         elif request.method == 'GET':
+            self.logger_rpc.log(self.name, self.reaction_handler.__name__, str(request), "Info", "Getting reaction")
             # Whether we need all user reactions or for specific event
             all_data = False
             try:
@@ -342,9 +358,13 @@ class Gateway:
                     favs = {"value": favs}
                 return self._cors_response(Response(json.dumps(favs), 200), '*', 'DELETE, GET, POST, OPTIONS')
         elif request.method == 'DELETE':
+            self.logger_rpc.log(self.name, self.reaction_handler.__name__, str(request), "Info", "Removing reaction")
             event_id = content['event_id']
-            if reaction_type == 'like':
-                self.likes_rpc.cancel_like([user, event_id])
-            elif reaction_type == 'favorite':
-                self.favorites_rpc.cancel_fav([user, event_id])
+            try:
+                if reaction_type == 'like':
+                    self.likes_rpc.cancel_like([user, event_id])
+                elif reaction_type == 'favorite':
+                    self.favorites_rpc.cancel_fav([user, event_id])
+            except:
+                self.logger_rpc.log(self.name, self.reaction_handler.__name__, str(request), "Error", "Can't remove reaction")
             return self._cors_response(Response(json.dumps({"message": "Reaction removed"}), 200), '*', 'DELETE, GET, POST, OPTIONS')
